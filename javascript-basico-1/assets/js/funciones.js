134129eb3eea1352a493d8cdefb4669af4064b47
Sergio Reinoso <slrs993@gmail.com>
2023-03-16 20:47:43 -0500

    Finalizacion de fundamentos de JavaScript seccion 3

--- javascript-basico-1/assets/js/funciones.js
+++ javascript-basico-1/assets/js/funciones.js
@@ -1,7 +1,10 @@
-
 function saludar(nombre){
-    console.log(arguments);
-    console.log('Hola ' + nombre);
+    // console.log(arguments);
+    // console.log('Hola ' + nombre);
+    return [1,2];
+
+    //Esto nunca se va a ejecutar
+    console.log('Soy un código que esta después del return');
 }
 
 const saludar2 = function(nombre){
@@ -16,7 +19,28 @@ const saludarFlecha2 = (nombre) => {
     console.log('Hola ' + nombre);
 }
 
-saludar('Sergio', 40, true, 'España');
+const retornoDeSaludar = saludar('Sergio', 40, true, 'España');
+console.log(retornoDeSaludar[0], retornoDeSaludar[1]);
 saludar2('Fernando');
-saludarFlecha();
-saludarFlecha2('Melissa');
+// saludarFlecha();
+// saludarFlecha2('Melissa');
+
+function sumar(a,b){
+    return a + b;
+}
+
+// const sumar2 = (a,b)=>{
+//     return a+b;
+// }
+const sumar2 = (a,b) => a + b;
+
+function getAleatorio(){
+    return Math.random();
+}
+
+//En una funcion de flecha, que no tenga llaves {}
+//getAleatorio2()
+const getAleatorio2 = () => Math.random();
+
+console.log(getAleatorio());
+console.log(getAleatorio2());
