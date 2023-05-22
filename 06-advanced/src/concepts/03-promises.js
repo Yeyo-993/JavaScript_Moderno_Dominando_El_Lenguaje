4481ebfbea260bfd3fb3da9ff727515e9db11476
Sergio Reinoso <slrs993@gmail.com>
2023-05-21 22:43:08 -0500

    Promise Hell

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -10,6 +10,15 @@ export const promiseComponent = ( element ) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeroes = (hero1, hero2) => {
+
+        element.innerHTML = `
+            <h3>${ hero1.name }</h3>
+            <h3>${ hero2.name }</h3>
+        `;
+
+    }
+
     const renderError = ( error ) => {
         element.innerHTML = `
             <h1>Error:</h1>
@@ -18,10 +27,18 @@ export const promiseComponent = ( element ) => {
     }
 
     const id1 = '5d86371f25a058e5b1c8a65e';
+    const id2 = '5d86371f233c9f2425f16916';
 
     findHero( id1 )
-        .then( renderHero )
+        .then( (hero1) => {
+            findHero(id2)
+                .then(hero2 => {
+                    renderTwoHeroes(hero1, hero2)
+                })
+                .catch( renderError );
+        })
         .catch( renderError );
+
 }
 
 /**
@@ -32,7 +49,7 @@ export const promiseComponent = ( element ) => {
 const findHero = ( id ) => {
 
     return new Promise(( resolve, reject ) => {
-        
+
         const hero = heroes.find( hero => hero.id === id );
 
         if ( hero ) {
