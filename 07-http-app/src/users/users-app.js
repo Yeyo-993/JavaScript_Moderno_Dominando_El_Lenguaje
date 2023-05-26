ba008168236607f7dbf49f0cb4160f4aaab5014a
Sergio Reinoso <slrs993@gmail.com>
2023-05-25 22:02:03 -0500

    caso de uso crear usuario

--- 07-http-app/src/users/users-app.js
+++ 07-http-app/src/users/users-app.js
@@ -1,4 +1,9 @@
+import { renderAddButton } from "./presentation/render-add-button/render-add-button";
+import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderModal } from "./presentation/render-modal/render-modal";
+import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
+import { saveUser } from "./use-cases/save-user";
 
 /**
  * 
@@ -6,6 +11,16 @@ import usersStore from "./store/users-store";
  */
 export const UsersApp = async( element ) => {
 
-    element.innerHTML = 'Loding...';
+    // element.innerHTML = 'Loding...';
     await usersStore.loadNextPage();
+
+    renderTable( element );
+    renderButtons( element );
+    renderAddButton( element );
+    renderModal( element, async(userLike) => {
+        const user = await saveUser(userLike);
+        usersStore.onUserChanged(user);
+        renderTable();
+    });
+
 }
